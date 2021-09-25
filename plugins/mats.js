3477b2d2906fb1d583ea548c9b1b8500a0f3df45
BlackerMoskie <91326079+BlackerMoskie@users.noreply.github.com>
2021-09-25 16:47:29 +0530

    Update mats.js

--- plugins/mats.js
+++ plugins/mats.js
@@ -6,12 +6,12 @@ const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-    Asena.addCommand({pattern: '.bts', fromMe: true, desc: Lang.WP}, (async (message, match) => {
+    Asena.addCommand({pattern: 'bts', fromMe: true, desc: Lang.WP}, (async (message, match) => {
 
         var r_text = new Array ();
 
-        r_text[0] = "https://images.wallpaperscraft.com/image/trees_pines_lake_198439_4480x6720.jpg";
-        r_text[1] = "https://images.wallpaperscraft.com/image/trees_pines_mountains_160486_3648x5472.jpg";
+        r_text[0] = "https://dapuhy-api.herokuapp.com/api/randomimage/batues?apikey=SSSSSSS1D";
+        r_text[1] = "https://dapuhy-api.herokuapp.com/api/randomimage/batues?apikey=SSSSSSS1D";
               var i = Math.floor(2*Math.random())
 
         var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
