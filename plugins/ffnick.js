41fa1c484dcba8fa80b5ac14096943116cf5f99b
BlackerMoskie <91326079+BlackerMoskie@users.noreply.github.com>
2021-10-02 14:16:23 +0530

    Create ffnick.js

---
+++ plugins/ffnick.js
@@ -0,0 +1,22 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const got = require('got');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('weather');
+
+Asena.addCommand({pattern: 'playstore ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
+	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
+	const url = `https://api.zeks.me/api/sgplay?apikey=00oQhHnqcqFGofHbjsFtXZcKVWO&q=${match[1]}`;
+	try {
+		const response = await got(url);
+		const json = JSON.parse(response.body);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
+		'*🐱‍👤 ' + Lang.NAMEY +'* ```' + json.result[0].title + '```\n\n' + 
+		'*🐱‍👤 ' + Lang.INFO +'* ```' + json.result[0].whatsnew + '```\n\n\n' + 
+		'*🐱‍👤 ' + Lang.DOWNLOAD +':* ```' + json.result[0].url + '```\n', MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+	}
+});
