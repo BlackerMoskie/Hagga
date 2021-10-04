ad0489665a350e0e3ea050c6c9fce719a9284d5b
BlackerMoskie <91326079+BlackerMoskie@users.noreply.github.com>
2021-10-04 12:58:37 +0530

    Update manim.js

--- plugins/manim.js
+++ plugins/manim.js
@@ -1,23 +1,38 @@
-const Asena = require('../events');
+const Raone = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const Config = require('../config');
 
 const Language = require('../language');
-const Lang = Language.getString('wallpaper');
+const  Anim = "Get animation videos";
 
-if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'manim', fromMe: true, desc: Lang.WP}, (async (message, match) => {
+    Raone.addCommand({pattern: 'anim', fromMe: true, desc: Anim}, (async (message, match) => {
 
         var r_text = new Array ();
 
         r_text[0] = "https://dapuhy-api.herokuapp.com/api/anime/storyanime?apikey=SSSSSSS1D";
         r_text[1] = "https://dapuhy-api.herokuapp.com/api/anime/storyanime?apikey=SSSSSSS1D";
-              var i = Math.floor(2*Math.random())
+
+        var i = Math.floor(2*Math.random())
 
         var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.CAPTION_TXT})
+        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.video, {mimetype: Mimetype.mp4, caption: Config.CP_TEXT})
+
+    }));
+
+Raone.addCommand({pattern: 'anim', fromMe: false, desc: Anim}, (async (message, match) => {
+
+        var r_text = new Array ();
+
+        r_text[0] = "https://dapuhy-api.herokuapp.com/api/anime/storyanime?apikey=UO0AST2FICHDgVP";
+        r_text[1] = "https://dapuhy-api.herokuapp.com/api/anime/storyanime?apikey=UO0AST2FICHDgVP";
+
+        var i = Math.floor(2*Math.random())
+
+        var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.video, {mimetype: Mimetype.mp4, caption: Config.CP_TEXT})
 
     }));
